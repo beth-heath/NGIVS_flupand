cf985b684b2d7a7745728ee6dfa1a38dd00fca26
Beth Heath <bah40@cam.ac.uk>
2025-06-19 09:33:23 +0100

    hopefully correcting XKX

--- functions/flu_sim.R
+++ functions/flu_sim.R
@@ -170,6 +170,7 @@ one_flu <- function(
   }
   
   country_name <- ifelse(country=='XKX', 'Kosovo', countrycode(country, origin='iso3c', destination='country.name'))
+
   
   ## making contact matrix wrt aged population
   contact_matrix <- fcn_contact_matrix(
